3d51917621150d0c7c8cdd4c4258823eb6073089
Kevin <kevin.cruz@hetic.net>
2021-02-04 19:41:55 +0100

    BlackJack

--- casino/Blackjack/BlackJack_play.js
+++ casino/Blackjack/BlackJack_play.js
@@ -10,6 +10,8 @@ var Deck = new Array();
 
 
 
+
+
 // Je me suis inspirer de https://www.thatsoftwaredude.com/content/6417/how-to-code-blackjack-using-javascript
 function initdeck (){
     let deck = [];
@@ -73,50 +75,55 @@ function PickCard (deck, Main){
 function SommeCarte(main){
     let somme = 0;
     for(let i = 0; i < main.length; i++){
-        somme += main["Valeur"];
+        somme += main[i]["Valeur"];
     }
     return somme;
 }
 
 function BlackJack(main){
-    let a;
-    let b;
-    for(let i = 0;)
+    let a = 11;
+    let b = 10;
+    carte1 = main[0]["Valeur"];
+    carte2 = main[1]["Valeur"];
+    if (carte1 == a && carte2 == b || carte1 == b && carte2 == a ){
+        return true;
+    }
+    
 
 }
 
 
-function Win(MainJ, MainB){
-    sommeJ = SommeCarte(MainJ)
-    sommeO = SommeCarte(MainB)
-    switch (sommeJ){
+// function Win(MainJ, MainB){
+//     sommeJ = SommeCarte(MainJ)
+//     sommeO = SommeCarte(MainB)
+//     switch (sommeJ){
 
-        case sommeJ == 21 :
-            alert("Vous avez gagné !");
-            break; 
-        case sommeJ > 21 :
-            alert("Vous avez perdu");
-            break;
-        default :
-            console.log("continue");
-            alert("ERROR"); 
-            break;   
-    }
+//         case sommeJ == 21 :
+//             alert("Vous avez gagné !");
+//             break; 
+//         case sommeJ > 21 :
+//             alert("Vous avez perdu");
+//             break;
+//         default :
+//             console.log("continue");
+//             alert("ERROR"); 
+//             break;   
+//     }
 
-    switch (sommeO){
+//     switch (sommeO){
 
-        case sommeJ == 21 :
-            alert("Vous avez perdu !");
-            break; 
-        case sommeJ > 21 :
-            alert("Vous avez gagné !");
-            break;
-        default :
-            console.log("continue");
-            alert("Error");
-            break;      
-    }
-}
+//         case sommeJ == 21 :
+//             alert("Vous avez perdu !");
+//             break; 
+//         case sommeJ > 21 :
+//             alert("Vous avez gagné !");
+//             break;
+//         default :
+//             console.log("continue");
+//             alert("Error");
+//             break;      
+//     }
+// }
 
 function Distrib(unDeck, MainDuJoueur, MainBanque){
     
@@ -133,23 +140,72 @@ var Deck=initdeck();
 
 var jouer = document.querySelector("#jouer");
 var tirer = document.querySelector("#tirer");
-var secoucher = document.querySelector("#secoucher");
+var secoucher = document.querySelector("#rester");
 
 Deck=initdeck();
-MixDeck(Deck);
-console.log(Deck);
-Distrib(Deck, MainOrdi, MainJoueur);
-console.log(MainJoueur);
-console.log(MainOrdi);
+console.log("Deck : ",Deck);
+ Distrib(Deck, MainOrdi, MainJoueur);
+ console.log("main du Joueur : ", MainJoueur);
+ console.log("main de la Banque : ", MainOrdi);
 
 
 tirer.addEventListener('click', () => {
+
     PickCard(Deck,MainJoueur);
-    console.log(Deck);
-    console.log(MainJoueur);
 
-})
+    console.log("Deck : ",Deck);
+    console.log("Vous avez maintenant : ", MainJoueur);
 
-tirer.addEventListener('click', () => {
- 
+    let sommeJ = SommeCarte(MainJoueur);
+
+    if(sommeJ > 21){
+        console.log("Perdu ! Vous avez plus de 21. ");
+    };
+    
+});
+
+secoucher.addEventListener('click', () => {
+
+    let sommeJ = SommeCarte(MainJoueur);
+    let sommeB = SommeCarte(MainOrdi);
+
+    while(sommeB < 17){
+        PickCard(Deck, MainOrdi);
+        console.log("La banque a maintenant : ", MainOrdi);
+        sommeB = SommeCarte(MainOrdi);
+    };
+
+    if(sommeB > 21){
+        console.log(" Gagné ! la banque à plus de 21.");
+    };
+
+    if(BlackJack(MainOrdi) && BlackJack(MainJoueur)){
+
+        console.log("Egalité ! Vous avez tout les deux Black Jack.");
+
+    }else if(BlackJack(MainOrdi) && !BlackJack(MainJoueur)){
+
+        console.log("Black Jack pour la Banque. Perdu !");
+
+    }else if (BlackJack(MainJoueur) && !BlackJack(MainOrdi)){
+
+        console.log("Black Jack pour le Joueur. C'est gagné !");
+
+    }else if (!BlackJack(MainOrdi) && !BlackJack(MainOrdi)){
+
+        if(sommeJ < sommeB && sommeB < 22 ){
+
+            console.log("La Banque a un plus grand jeu que vous. Perdu !");
+
+        }else if (sommeJ > sommeB){
+
+            console.log("C'est Gagné ! Vous avez un plus gros jeu");
+
+        }else if(sommeJ == sommeB){
+
+            console.log(" C'est une égalité ! Vos points sont égaux");
+
+        };
+
+    };
 })
